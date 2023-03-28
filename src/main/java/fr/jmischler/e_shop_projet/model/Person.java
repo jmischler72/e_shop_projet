3544497fdfef3eb5875ff1cc62adc407d4c35486
Joffrey <joffrey.mischler@tiime.fr>
2023-03-28 10:50:46 +0200

    first commit

---
+++ src/main/java/fr/jmischler/e_shop_projet/model/Person.java
@@ -0,0 +1,32 @@
+package fr.jmischler.e_shop_projet.model;
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
---
+++ src/test/java/fr/jmischler/e_shop_projet/EShopProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jmischler.e_shop_projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopProjetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
