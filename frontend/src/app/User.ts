bfa6293859f69b2458c6fc48591487da90e67509
Joffrey <joffrey.mischler@tiime.fr>
2023-04-13 18:30:01 +0200

    login functional

---
+++ frontend/src/app/User.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  pictureUrl: string;
+  category: string;
+
+  // all arguments constructor
+}
--- frontend/src/app/ecommerce/ecommerce.component.ts
+++ frontend/src/app/ecommerce/ecommerce.component.ts
@@ -1,16 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductOrder} from "../ProductOrder";
+import {User} from "../User";
 
 @Component({
   selector: 'app-ecommerce',
   templateUrl: './ecommerce.component.html',
   styleUrls: ['./ecommerce.component.scss']
 })
-export class EcommerceComponent {
+export class EcommerceComponent implements OnInit{
 
   filter = "";
   shoppingCart: ProductOrder[] = [];
+  username: string|undefined;
 
+  ngOnInit(){
+    // @ts-ignore
+    this.username = JSON.parse(localStorage.getItem('currentUser')).email;
+    console.log(this.username);
+
+    // this.username= JSON.parse(localStorage.getItem('currentUser')).username;
+  }
 
 
   removeFilter() {
