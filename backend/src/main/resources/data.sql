95195bdf609f1550b8ada1392f64e6349ec7ab35
MartinBrevot <mbrevot@metalmakers.co>
2023-04-13 17:59:38 +0200

    fix user fixtures

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO user (email, password) VALUES ('admin@gmail.com', 'admin');
-INSERT INTO user (email, password) VALUES ('client1@orange.fr', 'client1');
-INSERT INTO user (email, password) VALUES ('client2@gmail.com', 'client2');
+INSERT INTO user (email, password) VALUES ('admin@gmail.com', '92668751'); --hashed password, original: admin
+INSERT INTO user (email, password) VALUES ('client1@orange.fr', '860587462'); --hashed password, original: client1
+INSERT INTO user (email, password) VALUES ('client2@gmail.com', '860587463'); --hashed password, original: client2
 
 INSERT INTO product (name, price, stock, picture_url, category) VALUES ('Animal Crossing: New Horizons', 59.99, 10, 'https://fs-prod-cdn.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_5/SQ_NSwitch_AnimalCrossingNewHorizons_image500w.jpg', 'ADVENTURE');
 INSERT INTO product (name, price, stock, picture_url, category) VALUES ('Mario Kart 8 Deluxe', 69.99, 5, 'https://fs-prod-cdn.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_5/SQ_NSwitch_MarioKart8Deluxe_image500w.jpg', 'RACING');
