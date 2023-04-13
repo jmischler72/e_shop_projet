cbccf5ee972b68d7e8839a47a618fbe4ceab0f2e
MartinBrevot <mbrevot@metalmakers.co>
2023-04-13 17:24:48 +0200

    update fixtures

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -2,6 +2,7 @@ INSERT INTO user (email, password) VALUES ('admin@gmail.com', 'admin');
 INSERT INTO user (email, password) VALUES ('client1@orange.fr', 'client1');
 INSERT INTO user (email, password) VALUES ('client2@gmail.com', 'client2');
 
-INSERT INTO product (name, price, stock, picture_url, category) VALUES ('Super Mario', 30.0, 10, 'test', 'ADVENTURE');
-INSERT INTO product (name, price, stock, picture_url, category) VALUES ('Mario Kart', 20.0, 5, '', 'ARCADE');
-INSERT INTO product (name, price, stock, picture_url, category) VALUES ('Super Mario 64', 50.0, 2, '', 'ADVENTURE');
+INSERT INTO product (name, price, stock, picture_url, category) VALUES ('Animal Crossing: New Horizons', 59.99, 10, 'https://fs-prod-cdn.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_5/SQ_NSwitch_AnimalCrossingNewHorizons_image500w.jpg', 'ADVENTURE');
+INSERT INTO product (name, price, stock, picture_url, category) VALUES ('Mario Kart 8 Deluxe', 69.99, 5, 'https://fs-prod-cdn.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_5/SQ_NSwitch_MarioKart8Deluxe_image500w.jpg', 'RACING');
+INSERT INTO product (name, price, stock, picture_url, category) VALUES ('The Legend of Zelda: Tears of the Kingdom', 69.99, 2, 'https://fs-prod-cdn.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_5/1x1_NSwitch_TloZTearsOfTheKingdom_BASE_image500w.jpg', 'ADVENTURE');
+INSERT INTO product (name, price, stock, picture_url, category) VALUES ('New Super Mario Bros. U Deluxe', 59.99, 4, 'https://fs-prod-cdn.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_5/SQ_NSwitch_NewSuperMarioBrosUDeluxe_image500w.jpg', 'PLATEFORME');
